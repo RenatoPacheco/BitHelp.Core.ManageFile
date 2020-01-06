97e2edcf4d435417e1317fe822ea7c399b51694b
Renato Pacheco <renato.pacheco@adm.unip.br>
2020-01-06 12:11:10 -0300

    Add principal class

---
+++ src/CheckSUM.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace BitHelp.Core.ManageFile
+{
+    public static class CheckSUM
+    {
+        /// <summary>
+        /// Gets a hash of the file using MD5.
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <returns></returns>
+        public static string GetMD5Hash(string filePath)
+        {
+            if (System.IO.File.Exists(filePath))
+            {
+                using (var md5 = new MD5CryptoServiceProvider())
+                    return GetHash(filePath, md5);
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
+
+        /// <summary>
+        /// Gets a hash of the file using MD5.
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <returns></returns>
+        public static string GetMD5Hash(Stream s)
+        {
+            using (var md5 = new MD5CryptoServiceProvider())
+                return GetHash(s, md5);
+        }
+
+        private static string GetHash(string filePath, HashAlgorithm hasher)
+        {
+            using (var fs = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                return GetHash(fs, hasher);
+        }
+
+        private static string GetHash(Stream s, HashAlgorithm hasher)
+        {
+            var hash = hasher.ComputeHash(s);
+            var hashStr = Convert.ToBase64String(hash);
+            return hashStr.TrimEnd('=');
+        }
+    }
+}
