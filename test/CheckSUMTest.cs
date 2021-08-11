16f1211e1824b78a241b736b65d6b4055b8adc50
Renato Pacheco <renato.pacheco@outlook.com>
2021-08-11 13:11:14 -0300

    Using Test File Relative Reference

--- test/CheckSUMTest.cs
+++ test/CheckSUMTest.cs
@@ -1,17 +1,14 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using Xunit;
 
 namespace BitHelp.Core.ManageFile.Test
 {
     public class CheckSUMTest
     {
-        public readonly string _folder = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
-
         [Fact]
         public void Check_googlelogo_color_272x92dp()
         {
-            string file = Path.GetFullPath($@"{_folder}\Resources\googlelogo_color_272x92dp.png");
+            string file = Path.GetFullPath(@".\Resources\googlelogo_color_272x92dp.png");
             string result = CheckSUM.GetMD5Hash(file);
             Assert.Equal("j5Mn2yWX+lfS9CtKbFqYVQ", result);
         }
